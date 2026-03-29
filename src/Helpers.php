7f11ab0d4767f8634e8153e4598f06d5cd776084
Ersin KOÇ <ersinkoc@gmail.com>
2026-03-29 13:22:07 +0300

    remove unused formatDuration method and CodeMirror lazy-loading logic
    Remove Helpers::formatDuration() method and associated tests as duration formatting is not used in the application. Remove CodeMirror lazy-loading script and related CSS animations (toast-leave, slideOut) from frontend. Clean up unused CSS for CodeMirror editor styling

--- src/Helpers.php
+++ src/Helpers.php
@@ -13,11 +13,4 @@ final class Helpers {
         return round($size, 2) . ' ' . $units[$i];
     }
 
-    public static function formatDuration(int $seconds): string {
-        if ($seconds < 60) return "{$seconds}s";
-        if ($seconds < 3600) return floor($seconds / 60) . 'm ' . ($seconds % 60) . 's';
-        if ($seconds < 86400) return floor($seconds / 3600) . 'h ' . floor(($seconds % 3600) / 60) . 'm';
-        return floor($seconds / 86400) . 'd ' . floor(($seconds % 86400) / 3600) . 'h';
-    }
-
 }
